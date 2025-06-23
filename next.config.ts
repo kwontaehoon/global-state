fef4dd457d751ddc767a31d26196c30401333125
kwontaehoon <gju04195@gmail.com>
2025-06-23 14:08:46 +0900

    config: jotai-devtools

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactStrictMode: false
+  reactStrictMode: false,
+
+  // Learn more here - https://nextjs.org/docs/advanced-features/compiler#module-transpilation
+  // Required for UI css to be transpiled correctly 👇
+  transpilePackages: ['jotai-devtools'],
 };
 
 export default nextConfig;
